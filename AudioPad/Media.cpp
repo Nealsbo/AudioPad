1a1d5c1042e8891e5a9bf3a02e59744f75dcc7ca
Pavel Chernov <evolution1708@gmail.com>
2025-04-14 03:01:17 +0300

    Media objects split to diff files

---
+++ AudioPad/Media.cpp
@@ -0,0 +1,215 @@
+#include "Media.h"
+
+
+
+MediaPlayer::MediaPlayer() {}
+MediaPlayer::~MediaPlayer() {}
+
+void MediaPlayer::SetMedia(Mix_Music* m) {
+    playingMedia = m;
+    hasMedia = true;
+}
+
+void MediaPlayer::Eject() {
+    playingMedia = nullptr;
+    hasMedia = true;
+}
+
+void MediaPlayer::Play() {
+    printf("PLayer play\n");
+    if (playerState == MUS_STATE::PAUSED) {
+        playerState = MUS_STATE::PLAYING;
+        printf("PLayer resume\n");
+        Mix_ResumeMusic();
+    }
+    else {
+        playerState = MUS_STATE::PLAYING;
+        Mix_PlayMusic(playingMedia, 0);
+    }
+}
+
+void MediaPlayer::PlayNewMedia(Mix_Music* m) {
+    printf("PLayer play new\n");
+    if (playerState == MUS_STATE::PLAYING) {
+        Mix_HaltMusic();
+    }
+    playerState = MUS_STATE::PLAYING;
+    playingMedia = m;
+    hasMedia = true;
+    Mix_PlayMusic(playingMedia, 0);
+}
+
+void MediaPlayer::Stop() {
+    printf("PLayer stop\n");
+    if (playerState == MUS_STATE::PAUSED || playerState == MUS_STATE::PLAYING) {
+        playerState = MUS_STATE::STOPED;
+        Mix_HaltMusic();
+    }
+}
+
+void MediaPlayer::Pause() {
+    printf("PLayer pause\n");
+    if (playerState == MUS_STATE::PLAYING) {
+        playerState = MUS_STATE::PAUSED;
+        Mix_PauseMusic();
+    }
+}
+
+void MediaPlayer::FlipPlayPause() {
+    if (playerState == MUS_STATE::PLAYING) {
+        Pause();
+    }
+    else if (playerState == MUS_STATE::PAUSED || playerState == MUS_STATE::STOPED) {
+        Play();
+    }
+}
+
+void MediaPlayer::RewindTo(int value) {
+    if (playerState == MUS_STATE::PAUSED || playerState == MUS_STATE::PLAYING) {
+        Mix_SetMusicPosition((double)value);
+    }
+}
+
+int MediaPlayer::GetPlayingMediaTime() {
+    return (int)Mix_GetMusicPosition(playingMedia);
+}
+
+void MediaPlayer::SetMediaHotKey(const HotKeyData& h) {
+    hotKey.mod = h.mod;
+    hotKey.keycode = h.keycode;
+}
+
+void MediaPlayer::Update() {
+    if (playerState == MUS_STATE::PLAYING && !Mix_PlayingMusic())
+        playerState = MUS_STATE::STOPED;
+}
+
+
+
+
+PlayList::PlayList(MediaPlayer* pl) {
+    player = pl;
+}
+
+PlayList::~PlayList() {
+    player = nullptr;
+}
+
+void PlayList::LoadPlayList() {
+    std::string resDirName = openMediaFilesDialog(playList);
+
+    if (!resDirName.empty()) {
+        sprintf_s(playListDirC, "%s\0", resDirName.c_str());
+        playListDir = playListDirC;
+        activeMedia = &playList[0];
+        player->SetMedia(activeMedia->file);
+        currentMediaName = activeMedia->name;
+    }
+}
+
+void PlayList::ClearPlayList() {
+    activeMedia = nullptr;
+    currentMediaName = "none";
+    playListDir = "";
+    hotkeyAssigns.clear();
+    playList.clear();
+}
+
+bool PlayList::AssignHotkey(int id, const HotKeyData& hotkey) {
+    if (hotkeyAssigns.find(hotkey) != hotkeyAssigns.end()) {
+        hotkeyAssigns.emplace(hotkey, id);
+        return true;
+    }
+    return false;
+}
+
+void PlayList::RemoveHotkey(int key, int mod) {
+    HotKeyData hk{ mod, key };
+    if (hotkeyAssigns.find(hk) != hotkeyAssigns.end()) {
+        hotkeyAssigns.erase(hotkeyAssigns.find(hk));
+    }
+}
+
+void PlayList::PlayByHotkey(int key, int mod) {
+    HotKeyData hk{ mod, key };
+    if (hotkeyAssigns.find(hk) != hotkeyAssigns.end()) {
+        Media* playMedia = &playList[hotkeyAssigns[hk]];
+        player->PlayNewMedia(playMedia->file);
+    }
+}
+
+void PlayList::AssignPlayer(MediaPlayer* pl) {
+    player = pl;
+}
+
+
+void addNewMedia(std::vector<Media>& mediaList, const char* mediaPath) {
+    Media audio;
+    static int id = 1;
+    Mix_Music* m = Mix_LoadMUS(mediaPath);
+
+    if (!m) {
+        printf("Mix_LoadMUS error: %s\n", Mix_GetError());
+        return;
+    }
+
+    std::string pathstr = mediaPath;
+    std::size_t subpos = pathstr.find_last_of("\\");
+
+    audio.path = mediaPath;
+    audio.name = pathstr.substr(subpos + 1);
+    audio.length = Mix_MusicDuration(m);
+    audio.file = m;
+    audio.ID = id;
+    id++;
+    mediaList.push_back(audio);
+}
+
+std::string openMediaFilesDialog(std::vector<Media>& mediaList) {
+    const nfdpathset_t* filePathes;
+
+    std::string dirName;
+
+    nfdu8filteritem_t filters[2] = { { "Media", "mp3,wav,flac" } };
+    nfdopendialogu8args_t args = { 0 };
+    args.filterList = filters;
+    args.filterCount = 1;
+
+    nfdresult_t result = NFD_OpenDialogMultipleU8_With(&filePathes, &args);
+    if (result == NFD_OKAY) {
+        puts("Success!");
+
+        nfdpathsetsize_t numPaths;
+        NFD_PathSet_GetCount(filePathes, &numPaths);
+
+        nfdpathsetsize_t i;
+        if (numPaths) {
+            nfdchar_t* path;
+            NFD_PathSet_GetPath(filePathes, 0, &path);
+            std::string pathstr = path;
+            std::size_t subpos = pathstr.find_last_of("\\");
+            dirName = pathstr.substr(0, subpos);
+
+            printf("Path %i: %s\n", 0, path);
+        }
+        for (i = 0; i < numPaths; ++i) {
+            nfdchar_t* path;
+            NFD_PathSet_GetPath(filePathes, i, &path);
+            printf("Path %i: %s\n", (int)i, path);
+            addNewMedia(mediaList, path);
+
+            NFD_PathSet_FreePath(path);
+        }
+    }
+    else if (result == NFD_CANCEL) {
+        puts("User pressed cancel.");
+    }
+    else {
+        printf("Error: %s\n", NFD_GetError());
+    }
+
+    for (auto& m : mediaList) {
+        printf("name: %s\n", m.name.c_str());
+    }
+    return dirName;
+}
---
+++ AudioPad/Media.h
@@ -0,0 +1,103 @@
+#pragma once
+
+#include <SDL.h>
+#include <SDL_mixer.h>
+
+#include <string>
+#include <vector>
+#include <unordered_map>
+
+#include "nfd.h"
+
+struct Media {
+    Media() {}
+    ~Media() {}
+
+    Mix_Music* file = nullptr;
+    std::string path = "";
+    std::string name = "blank";
+    std::string type = "mp3";
+    uint32_t length = 1;
+    int ID = 0;
+};
+
+enum MUS_STATE {
+    IDLE = 0,
+    PLAYING,
+    STOPED,
+    PAUSED
+};
+
+enum MUS_CMD {
+    PLAY = 1,
+    STOP,
+    PAUSE
+};
+
+void addNewMedia(std::vector<Media>& mediaList, const char* mediaPath);
+std::string openMediaFilesDialog(std::vector<Media>& mediaList);
+
+struct HotKeyData {
+    bool operator==(const HotKeyData& h) const {
+        return mod == h.mod && keycode == h.keycode;
+    }
+
+    int mod = 0;
+    int keycode = 0;
+};
+
+template<>
+struct std::hash<HotKeyData> {
+    size_t operator()(const HotKeyData& h) const {
+        return (hash<SDL_Keycode>()(h.keycode) ^ (hash<Uint16>()(h.mod) << 1));
+    }
+};
+
+struct MediaPlayer {
+    MediaPlayer();
+    ~MediaPlayer();
+
+    void SetMedia(Mix_Music* m);
+    void Eject();
+    void Play();
+    void PlayNewMedia(Mix_Music* m);
+    void Stop();
+    void Pause();
+    void FlipPlayPause();
+    void RewindTo(int value);
+    int GetPlayingMediaTime();
+    void SetMediaHotKey(const HotKeyData& h);
+    void Update();
+
+    Mix_Music* playingMedia = nullptr;
+    int playerState = IDLE;
+    bool hasMedia = false;
+    HotKeyData hotKey;
+};
+
+struct PlayList {
+    PlayList(MediaPlayer* pl = nullptr);
+    ~PlayList();
+
+    void LoadPlayList();
+    void ClearPlayList();
+
+    bool AssignHotkey(int id, const HotKeyData& hotkey);
+
+    void RemoveHotkey(int key, int mod);
+    void PlayByHotkey(int key, int mod);
+
+    void AssignPlayer(MediaPlayer* pl);
+
+    void CheckOnPlayer();
+
+    char playListDirC[128] = { 0 };
+    std::string playListDir = "";
+
+    std::vector<Media> playList;
+    std::unordered_map<HotKeyData, int> hotkeyAssigns;
+
+    MediaPlayer* player = nullptr;
+    std::string currentMediaName = "none";
+    Media* activeMedia = nullptr;
+};
